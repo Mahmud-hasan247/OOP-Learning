87e2045566751fc444857aa37be471afca599d77
Intern  IT 38 <intern38.it@akij.net>
2021-01-19 16:23:41 +0600

    some code added

--- oop-1.js
+++ oop-1.js
@@ -14,40 +14,79 @@
 // console.log(firstName)
 // "use strict"
 
-function Person(firstName, lastName) {
-  //constructor functiond
-  this.firstName = firstName;
-  this.lastName = lastName;
-  // this.fullName = function(){
-  //     return `${this.firstName} ${this.lastName}`;
-  // }
-  // console.log(this)
+// function Person(firstName, lastName) {
+//   //constructor functiond
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   // this.fullName = function(){
+//   //     return `${this.firstName} ${this.lastName}`;
+//   // }
+//   // console.log(this)
+// }
+// // console.dir(Person)
+// Person.prototype.getFullName = function () {
+//   console.log(`${this.firstName} ${this.lastName}`);
+// };
+// // const newPerson = new Person("Masnun", "hasan");
+// // console.log(Person.prototype == newPerson.__proto__)
+
+// // Inheritance
+// function NewInfo(firstName, lastName, profession) {
+//   Person.call(this, firstName, lastName);
+//   this.profession = profession;
+// }
+
+// const newProfession = new NewInfo("Mahmud", "Hasan", "Study");
+// console.log(newProfession);
+
+// NewInfo.prototype.getAllInfo = function () {
+//   return `${this.firstName} ${this.lastName} ${this.profession}`;
+// };
+
+// NewInfo.prototype.__proto__ = Person.prototype;
+// console.log(NewInfo.prototype.__proto__ = Person.prototype);
+
+// console.log(newProfession.getAllInfo());
+// const PersonOne = new Person("mahmud", "hasan");
+// const PersonTwo = new Person("mahdi", "hasan");
+// console.log(PersonOne);
+// console.log(PersonTwo);
+// console.log(PersonOne.getFullName());
+// console.log(PersonTwo.getFullName());
+// PersonOne.getFullName();
+// newProfession.getFullName();
+
+// Creating Objects
+// const Obj = {
+//   init: function (firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     return this;
+//   },
+//   fullName: function () {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
+// Obj.init("mahmud", "Hasan");
+// const Obj = new Object();
+// Obj.firstName = "mahmud"
+// Obj.lastName = "hasan"
+
+const Person = {
+    firstName: "Tahsin",
+    lastName: "hasan"
 }
-// console.dir(Person)
-Person.prototype.getFullName = function () {
-  console.log(`${this.firstName} ${this.lastName}`);
-};
-// const newPerson = new Person("Masnun", "hasan");
-// console.log(Person.prototype == newPerson.__proto__)
 
-// Inheritance
-function NewInfo(firstName, lastName, profession) {
-  Person.call(this, firstName, lastName);
-  this.profession = profession;
-}
-
-const newProfession = new NewInfo("Mahmud", "Hasan", "Study");
-console.log(newProfession);
-
-NewInfo.prototype.getAllInfo = function () {
-  return `${this.firstName} ${this.lastName} ${this.profession}`;
-};
-console.log(newProfession.getAllInfo());
-
-const PersonOne = new Person("mahmud", "hasan");
-const PersonTwo = new Person("mahdi", "hasan");
-console.log(PersonOne);
-console.log(PersonTwo);
-console.log(PersonOne.getFullName());
-console.log(PersonTwo.getFullName());
-PersonOne.getFullName();
+const Obj = Object.create(
+  Person,
+  {
+    firstName: {
+      value: "mahdi",
+    },
+    // lastName: {
+    //   value: "hasan",
+    // },
+  }
+);
+console.log(Obj)
+console.log(Obj.firstName);
